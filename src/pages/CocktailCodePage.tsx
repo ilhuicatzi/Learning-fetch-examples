ee6b0f36cd42a40def9eea727cfac178e01ee706
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2024-07-27 02:03:47 -0600

    Change a for Link

--- src/pages/CocktailCodePage.tsx
+++ src/pages/CocktailCodePage.tsx
@@ -1,14 +1,15 @@
 import CodeComponent from "@/components/code/CodeComponent"
 import { CocktailCode } from "@/utils/codeExamples"
 import { BackIcon } from "@/assets/icons/BackIcon"
+import { Link } from "react-router-dom"
 
 function CocktailCodePage() {
   return (
     <section className="mt-5 w-11/12 px-10">
     <div className="flex items-center gap-3">
-        <a href="/cocktail" className="flex items-center text-yellow-500 hover:bg-accent p-1 rounded-lg">
+        <Link to="/cocktail" className="flex items-center text-yellow-500 hover:bg-accent p-1 rounded-lg">
             <BackIcon className="text-2xl" />
-        </a>
+        </Link>
         <h2 className="text-3xl marker:font-mono text-yellow-500"> Código de la página cocteles</h2>
     </div>
     <CodeComponent codeString={ CocktailCode } />
--- src/pages/RandomUsersCodePage.tsx
+++ src/pages/RandomUsersCodePage.tsx
@@ -1,14 +1,15 @@
 import CodeComponent from "@/components/code/CodeComponent"
 import { RandomUsersCode } from "@/utils/codeExamples"
 import { BackIcon } from "@/assets/icons/BackIcon"
+import { Link } from "react-router-dom"
 
 function RandomUsersCodePage() {
   return (
     <section className="mt-5 w-11/12 px-10">
     <div className="flex items-center gap-3">
-        <a href="/random-users" className="flex items-center text-sky-500 hover:bg-accent p-1 rounded-lg">
+        <Link to="/random-users" className="flex items-center text-sky-500 hover:bg-accent p-1 rounded-lg">
             <BackIcon className="text-2xl" />
-        </a>
+        </Link>
         <h2 className="text-3xl marker:font-mono text-sky-500"> Código de la página Random Users</h2>
     </div>
     <CodeComponent codeString={ RandomUsersCode } />
